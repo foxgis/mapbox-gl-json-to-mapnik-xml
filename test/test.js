621d2f2b73f72b1c6ee5628c39ff381e74dd9027
jennica <962464538@qq.com>
2016-07-13 10:06:08 +0800

    update tests

--- test/test.js
+++ test/test.js
@@ -1,13 +1,15 @@
 var fs=require('fs');
 var mgl2xml=require('../index');
 
-fs.readFile('test/files/admin-v8-2.json', 'utf-8', function (err, data) {
+fs.readFile('./files/foxgis-admin.json', 'utf-8', function (err, data) {
     if (err) console.log(err);
     else {
         var obj = JSON.parse(data.toString());
-
-        mgl2xml.gl2xml(obj, function (err, result) {
-            console.log(result.toString());
+        mgl2xml(obj, function (err, result) {
+            fs.writeFile('files/result.xml',result,function(err,data){
+                if(err){console.log(err);}
+                else{console.log('ok');}
+            })
         });
     }
 });
