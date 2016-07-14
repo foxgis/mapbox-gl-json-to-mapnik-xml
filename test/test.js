8a422734697b314eaf2166eb0c6adc93cead491b
jingsam <jingsam@163.com>
2016-07-14 21:53:06 +0800

    add test

--- test/test.js
+++ test/test.js
@@ -1,15 +1,35 @@
-var fs=require('fs');
-var mgl2xml=require('../index');
+var fs = require('fs')
+var tilelive = require('tilelive')
+var Vector = require('tilelive-vector')
+var Http = require('tilelive-http')
+var gl2xml = require('../index')
+var style = require('./fixtures/style')
 
-fs.readFile('test/files/foxgis-admin.json', 'utf-8', function (err, data) {
-    if (err) console.log(err);
-    else {
-        var obj = JSON.parse(data.toString());
-        mgl2xml(obj, function (err, result) {
-            fs.writeFile('test/files/result.xml',result,function(err,data){
-                if(err){console.log(err);}
-                else{console.log('ok');}
-            })
-        });
-    }
-});
+
+Vector.registerProtocols(tilelive)
+Http(tilelive)
+
+gl2xml(style, function(err, xml) {
+  if (err) return console.log(err)
+
+  fs.writeFileSync('./fixtures/result.xml', xml)
+
+  var uri = {
+    protocol: 'vector:',
+    xml: xml,
+    scale: opts.scale,
+    format: opts.format
+  }
+
+  tilelive.load(uri, function(err, source) {
+    if (err) return console.log(err)
+
+    source.getTile(0, 0, 0, function(err, data, headers) {
+      if (err) return console.log(err)
+
+      console.log(headers)
+      console.log(data.length)
+      fs.writeFileSync('0-0-0.png', data)
+    })
+  })
+})
